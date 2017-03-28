f2e4b1d89612ecf0577f64827dac53fd8e4ab668
Yui Kojima <y525dlm9@gmail.com>
2017-03-28 06:48:16 +0000

    first commit

---
+++ test-carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+            loop: true,
+            nav: false,
+            items: 1,
+            autoplay: true
+    });
+});
